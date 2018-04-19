c0c2611167d231adfe50bccf2ee1f73969bfe386
Ray, Jason D ERDC-RDE-ITL-MS <jaray200@gmail.com>
2018-04-19 08:46:14 -0500

    A few more figures added

--- CompareWaveletToAveraged.m
+++ CompareWaveletToAveraged.m
@@ -1,7 +1,7 @@
 %Uncomment below if it is the first time loading
-% clear;
-%load('..\Data\Westpoint\WestpointUnAveraged.mat');
-%load('..\Data\Westpoint\WestpointAveraged.mat');
+ clear;
+load('..\Data\Westpoint\WestpointUnAveraged.mat');
+load('..\Data\Westpoint\WestpointAveraged.mat');
 
 fs = 62.5e6;
 x = ((0:length(WestpointAveraged(1).raw)-1)./fs).*1000;
